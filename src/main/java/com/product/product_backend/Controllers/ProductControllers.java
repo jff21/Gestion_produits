431aac8c2f55aa51876303aab3dbd37b23d79827
jff21 <91060673+jff21@users.noreply.github.com>
2023-01-05 02:08:48 +1400

    filter add

--- src/main/java/com/product/product_backend/Controllers/ProductControllers.java
+++ src/main/java/com/product/product_backend/Controllers/ProductControllers.java
@@ -1,23 +1,21 @@
 package com.product.product_backend.Controllers;
 
 import com.product.product_backend.DTOS.ProductDTO;
-import com.product.product_backend.DTOS.SearchContext;
-import com.product.product_backend.Entities.Media;
 import com.product.product_backend.Entities.Product;
 import com.product.product_backend.Repositories.MediaRepository;
+import com.product.product_backend.Repositories.ProductRepo;
 import com.product.product_backend.Services.ProductService;
+import com.product.product_backend.Specification.ProductSpecifications;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 
 @Slf4j
@@ -25,11 +23,11 @@ import java.util.Set;
 @RequestMapping("/product")
 public class ProductControllers {
     private final ProductService productService;
-    private final MediaRepository mediaRepository;
+    private final ProductRepo productRepo;
 
-    public ProductControllers(ProductService productService, MediaRepository mediaRepository) {
+    public ProductControllers(ProductService productService, MediaRepository mediaRepository, ProductRepo productRepo) {
         this.productService = productService;
-        this.mediaRepository = mediaRepository;
+        this.productRepo = productRepo;
     }
 
     @PostMapping(value={"/create"},consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
@@ -50,7 +48,7 @@ public class ProductControllers {
         return productService.getProd(prod_id);
     }
 
-    @GetMapping("/Allprod")
+    @GetMapping("/All_prod")
     public List<Product> Allprod(){
         log.info("getting all prod.....");
         return productService.Allproduct();
@@ -72,4 +70,11 @@ public class ProductControllers {
                                                   @RequestParam(value = "reference",required = false) String reference){
         return new ResponseEntity<>(productService.Search(mark, designation, reference),HttpStatus.OK);
    }
+
+    @GetMapping("/filter")
+    public List<Product> Filter(@RequestParam(value = "mark",required = false) String mark, @RequestParam(value = "designation",required = false) String designation,
+                                                   @RequestParam(value = "reference",required = false) String reference){
+        Specification<Product> specification = ProductSpecifications.getSpec(mark, designation, reference);
+        return productRepo.findAll(specification);
+    }
 }
---
+++ src/main/java/com/product/product_backend/Specification/ProductSpecifications.java
@@ -0,0 +1,26 @@
+package com.product.product_backend.Specification;
+
+import com.product.product_backend.Entities.Product;
+import org.springframework.data.jpa.domain.Specification;
+
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductSpecifications {
+
+    public static Specification<Product>getSpec(String mark, String designation, String reference) {
+        return ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (mark != null && !(mark.isEmpty()))
+                predicates.add(criteriaBuilder.equal(root.get("mark"), mark));
+            if(designation!= null && !(designation.isEmpty()))
+                predicates.add(criteriaBuilder.equal(root.get("designation"),designation));
+            if(reference!= null && !(reference.isEmpty()))
+                predicates.add(criteriaBuilder.equal(root.get("reference"),reference));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        });
+    }
+
+
+}
