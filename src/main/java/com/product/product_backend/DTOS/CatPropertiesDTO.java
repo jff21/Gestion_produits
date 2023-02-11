62cf3884f8ba3f60e6b58c7f7623952b943b1a51
jff21 <91060673+jff21@users.noreply.github.com>
2023-02-12 00:48:52 +1400

    product

--- src/main/java/com/product/product_backend/DTOS/CatPropertiesDTO.java
+++ src/main/java/com/product/product_backend/DTOS/CatPropertiesDTO.java
@@ -1,4 +1,18 @@
 package com.product.product_backend.DTOS;
 
-public class CatPropertiesDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.product.product_backend.Models.Category;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CatPropertiesDTO  {
+    private String range;
+    private String key;
+    private String name;
+    private String mark;
+    private String color;
+ @JsonProperty(required = true)
+    public Category category;
 }
--- src/main/java/com/product/product_backend/Repositories/CatPropertieRepo.java
+++ src/main/java/com/product/product_backend/Repositories/CatPropertieRepo.java
@@ -1,15 +1,19 @@
 package com.product.product_backend.Repositories;
 
-import com.product.product_backend.Models.Catproperties;
+import com.product.product_backend.Models.Cat_properties;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CatPropertieRepo extends JpaRepository<Catproperties,Long> {
+public interface CatPropertieRepo extends JpaRepository<Cat_properties,Long> {
 
-    @Query(value = "SELECT * FROM catproperties WHERE cat_id=:id" ,nativeQuery = true)
-    List<Catproperties> findAllByCategorie(Long id);
+    @Query(value = "SELECT * FROM catproperties WHERE categorie_cat_id=:id" ,nativeQuery = true)
+    List<Cat_properties> findAllByCategorie(Long id);
+
+    @Query(value = "SELECT cp FROM Cat_properties cp WHERE cp.category.cid = :cid")
+    List<Cat_properties> findByCategoryCid(@Param("cid") String cid);
 }
