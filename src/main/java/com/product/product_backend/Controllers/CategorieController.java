33f187ca2f1546e115593e9aa8c60da8959789fa
jff21 <91060673+jff21@users.noreply.github.com>
2023-02-12 00:40:12 +1400

    cat

--- src/main/java/com/product/product_backend/Controllers/CategorieController.java
+++ src/main/java/com/product/product_backend/Controllers/CategorieController.java
@@ -1,13 +1,16 @@
 package com.product.product_backend.Controllers;
 
 import com.product.product_backend.DTOS.CategorieDTO;
-import com.product.product_backend.Models.Categorie;
+import com.product.product_backend.DTOS.ResultprocatDTO;
+import com.product.product_backend.Models.Category;
+import com.product.product_backend.Repositories.CategorieRepo;
 import com.product.product_backend.Services.CategorieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,13 +19,35 @@ import java.util.Optional;
 public class CategorieController {
     private final CategorieService categorieService;
 
+    private final CategorieRepo categorieRepo;
+
     @PostMapping("/create")
-    public ResponseEntity<Categorie> createCat(@RequestBody CategorieDTO categorieDTO){
+    public ResponseEntity<Category> createCat(@RequestBody CategorieDTO categorieDTO){
         return new ResponseEntity<>(categorieService.createCat(categorieDTO), HttpStatus.CREATED);
     }
 
-    @GetMapping("/cat/{cat_id}")
-    public Optional<Categorie> getCat(@PathVariable(name = "cat_id") Long cat_id){
-        return categorieService.getCat(cat_id);
+    @GetMapping("/cat/{id}")
+    public Optional<Category> getCat(@PathVariable Long id){
+        return categorieService.getCat(id);
     }
+
+    @GetMapping("/all")
+    public List<Category> getCat(){
+        return categorieService.AllCat();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Category> updateCat(@RequestBody CategorieDTO categorieDTO){
+        return new ResponseEntity<>(categorieService.Update(categorieDTO), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/del/{id}")
+    public void delete(@PathVariable Long cat_id){
+        categorieService.delete(cat_id);
+    }
+
+//    @GetMapping("/getdetails/{cat_id}")
+//    public List<ResultprocatDTO> getDetails(@PathVariable Long cat_id){
+//      return   categorieRepo.getDetails(cat_id);
+//    }
 }
--- src/main/java/com/product/product_backend/DTOS/CategorieDTO.java
+++ src/main/java/com/product/product_backend/DTOS/CategorieDTO.java
@@ -1,18 +1,23 @@
 package com.product.product_backend.DTOS;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.product.product_backend.Models.Product;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class CategorieDTO {
 
     private Long cat_id;
-//    private Date creationDate;
-//    private Date lastUpdate;
+    private String cid;
+    private String key;
+    private Date createDate;
+    private Date lastUpdate;
     private String name;
-    private List<Product> product;
-    private Long  catproperties;
 
 }
