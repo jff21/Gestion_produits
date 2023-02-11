3713df6bee93c69f95215af6d7bfe8207a749671
jff21 <91060673+jff21@users.noreply.github.com>
2023-02-12 00:48:29 +1400

    product

--- src/main/java/com/product/product_backend/Models/Product.java
+++ src/main/java/com/product/product_backend/Models/Product.java
@@ -3,6 +3,7 @@ package com.product.product_backend.Models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.util.Set;
@@ -25,22 +26,26 @@ public class Product {
     private float minimumThreshold;
     private int unitPrice;
     private String reference;
-
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private Byte[] qrCode;
     @ManyToOne
-    private Categorie categorie;
+    private Category categorie;
     @ManyToOne
     private Level level;
     @ManyToOne
     private Unit unit;
     @ManyToOne
     private Productline productline;
+    @ManyToOne
+    private Iventory iventory;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "prod_media", joinColumns = {@JoinColumn(name = "prod_id")},
             inverseJoinColumns = {@JoinColumn(name = "media_id")})
     private Set<Media>  media;
-
-    @OneToOne
-    private QrCode qrCode;
+//
+//    @OneToOne
+//    private QrCode qrCode;
 
 }
