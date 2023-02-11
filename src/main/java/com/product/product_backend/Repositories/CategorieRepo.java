fe7caaecdabe103d27f275f72e90e8f38131f3fd
jff21 <91060673+jff21@users.noreply.github.com>
2023-02-12 00:41:05 +1400

    cat

--- src/main/java/com/product/product_backend/Repositories/CategorieRepo.java
+++ src/main/java/com/product/product_backend/Repositories/CategorieRepo.java
@@ -1,9 +1,25 @@
 package com.product.product_backend.Repositories;
 
-import com.product.product_backend.Models.Categorie;
+import com.product.product_backend.DTOS.ResultprocatDTO;
+import com.product.product_backend.Models.Cat_properties;
+import com.product.product_backend.Models.Category;
+import org.hibernate.sql.Select;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface CategorieRepo extends JpaRepository<Categorie,Long> {
+public interface CategorieRepo extends JpaRepository<Category,Long> {
+    @Query(value = "SELECT * FROM category WHERE cid =:cid",nativeQuery = true)
+    Category findByCid(String cid);
+
+
+//    @Query("SELECT new com.product.product_backend.DTOS.ResultprocatDTO" +
+//            "(c.cid,c.key,c.createDate,c.lastUpdate,c.name,cp.range,cp.mark,cp.color) " +
+//            "FROM Category c join c.cat_properties cp " +
+//            "where c.cat_id = :cat_id")
+//    List<ResultprocatDTO> getDetails(long cat_id);
+
 }
--- src/main/java/com/product/product_backend/Services/CategorieService.java
+++ src/main/java/com/product/product_backend/Services/CategorieService.java
@@ -1,17 +1,18 @@
 package com.product.product_backend.Services;
 
 import com.product.product_backend.DTOS.CategorieDTO;
-import com.product.product_backend.Models.Categorie;
+import com.product.product_backend.Models.Category;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategorieService {
 
-    Categorie createCat(CategorieDTO categorieDTO);
-    Categorie Update(CategorieDTO categorieDTO);
-    Optional<Categorie> getCat(Long cat_id);
-    List<Categorie> AllCat();
+    Category createCat(CategorieDTO categorieDTO);
+    Category Update(CategorieDTO categorieDTO);
+    Optional<Category> getCat(Long id);
+    List<Category> AllCat();
     void delete(Long cat_id);
+
 
 }
