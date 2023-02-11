b964bfbbdd8ebc06b54626d89b9e325c9f371fc1
jff21 <91060673+jff21@users.noreply.github.com>
2023-02-12 00:38:33 +1400

    cat_properties

--- src/main/java/com/product/product_backend/Models/Cat_properties.java
+++ src/main/java/com/product/product_backend/Models/Cat_properties.java
@@ -1,25 +1,30 @@
 package com.product.product_backend.Models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.io.Serializable;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Catproperties {
+public class Cat_properties {
     @Id
     @Column(nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int prop_id ;
+    private String key;
     private String range;
     private String name;
     private String mark;
-    private Date lastUpdate;
-    private Date creationDate;
     private String color;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category",referencedColumnName = "cid")
+    @JsonBackReference
+    public Category category;
 }
